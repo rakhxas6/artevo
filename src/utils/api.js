9aa081dbd454df2af534c7867e8eabdb85062953
Sudhan Kandel <sujan.kandel.0ne8@gmail.com>
2025-07-09 17:08:39 +0545

    made it work locally

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,24 +1,36 @@
 import axios from "axios";
 
-const params = {
-  headers: {
-    Authorization: "bearer " + process.env.REACT_APP_STRIPE_APP_KEY,
-  },
-};
+// const params = {
+//   headers: {
+//     Authorization: "bearer " + process.env.REACT_APP_STRIPE_APP_KEY,
+//   },
+// };
 
-export const fetchDataFromApi = async (url) => {
+export const fetchDataFromApi = async (
+  url,
+  extraParams = {
+    headers: {
+      Authorization: "bearer " + process.env.REACT_APP_STRIPE_APP_KEY,
+    },
+  }
+) => {
   try {
     const { data } = await axios.get(
       process.env.REACT_APP_STRIPE_APP_DEV_URL + url,
-      params
+      {
+        headers: {
+          Authorization: "bearer " + process.env.REACT_APP_STRIPE_APP_KEY,
+        },
+        timeout: 10000,
+        params: extraParams,
+      }
     );
     return data;
   } catch (err) {
-    console.log(err);
+    console.error("API error:", err);
     return err;
   }
 };
-
 
 export const makePaymentRequest = axios.create({
   baseURL: process.env.REACT_APP_STRIPE_APP_DEV_URL,
